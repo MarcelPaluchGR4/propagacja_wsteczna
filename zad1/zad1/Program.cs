975428081519c1ce06bfb88f8f4a28ccc26c3d23
MarcelPaluchGR4 <parcelmurzyn@gmail.com>
2025-05-10 11:54:43 +0200

    WIP

--- zad1/zad1/Program.cs
+++ zad1/zad1/Program.cs
@@ -1,10 +1,17 @@
 ﻿class Program
 {
+
     public class Neuron
     {
         public double input;
         public double output;
         public double bias;
-        public double error;
-    }
+    };
+
+    public class NeuralNetwork()
+    {
+        public int Epoch = 20000;
+        private Neuron[][] Neurons;
+    };
+
 }
