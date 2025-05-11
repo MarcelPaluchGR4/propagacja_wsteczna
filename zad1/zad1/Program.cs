acbfcaca242a0f6886a8bd43429ca5a673ba2ebf
MarcelPaluchGR4 <parcelmurzyn@gmail.com>
2025-05-11 13:26:40 +0200

    WIP

--- zad1/zad1/Program.cs
+++ zad1/zad1/Program.cs
@@ -2,7 +2,13 @@
 
 class Program
 {
-    static int numberOfNeurons = 2;
+    static int inputLayerNeurons = 2;
+    static int hiddenLayerNeurons = 2;
+    static int outputLayerNeurons = 1;
+    static double learningParameter = 0.3;
+    static int Epoch = 50000;
+
+    static double Sigmoid(double x) => 1.0 / (1.0 + Math.Exp(-x));
 
     class Neuron
     {
@@ -12,14 +18,40 @@ class Program
         public double bias;
     }
 
-    public class NeuralNetwork()
-    {
-        public int Epoch = 20000;
-    };
-
     static void Main()
     {
-        int[] layers = new[] { 2, 2, 1 };
+        Random rand = new Random();
+        int[] layers = [inputLayerNeurons, hiddenLayerNeurons, outputLayerNeurons];
+        var xorTrainingSet = new double[][]
+        {
+                [0, 0, 0],
+                [0, 1, 1],
+                [1, 0, 1],
+                [1, 1, 0]
+        };
+
+        double[,] weightsInputHidden = new double[hiddenLayerNeurons, inputLayerNeurons];
+        double[] biasHidden = new double[hiddenLayerNeurons];
+
+        double[] weightsHiddenOutput = new double[hiddenLayerNeurons];
+        double biasOutput = rand.NextDouble() * 2 - 1;
+
+        for (int i = 0; i < hiddenLayerNeurons; i++)
+        {
+            biasHidden[i] = rand.NextDouble() * 5 - 5;
+            weightsHiddenOutput[i] = rand.NextDouble() * 5 - 5;
+            for (int j = 0; j < inputLayerNeurons; j++)
+                weightsInputHidden[i, j] = rand.NextDouble() * 5 - 5;
+        }
+
+        for (int epoch = 0; epoch < Epoch; epoch++)
+        {
+            foreach (var data in xorTrainingSet)
+            {
+                double[] inputs = { data[0], data[1] };
+                double expected = data[2];
+            }
+        }
     }
 
 }
