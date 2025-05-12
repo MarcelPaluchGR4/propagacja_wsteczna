addd61207e73a5ed007bc46a5f71b4d58d3c9295
MarcelPaluchGR4 <parcelmurzyn@gmail.com>
2025-05-12 20:54:58 +0200

    WIP

--- zad1/zad1/Program.cs
+++ zad1/zad1/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.IO.Compression;
-
-class Program
+﻿class Program
 {
     static int inputLayerNeurons = 2;
     static int hiddenLayerNeurons = 2;
@@ -10,24 +8,16 @@ class Program
 
     static double Sigmoid(double x) => 1.0 / (1.0 + Math.Exp(-x));
 
-    class Neuron
-    {
-        public double input;
-        public double output;
-        public double error;
-        public double bias;
-    }
-
     static void Main()
     {
         Random rand = new Random();
         int[] layers = [inputLayerNeurons, hiddenLayerNeurons, outputLayerNeurons];
         var xorTrainingSet = new double[][]
         {
-                [0, 0, 0],
-                [0, 1, 1],
-                [1, 0, 1],
-                [1, 1, 0]
+            [0, 0, 0],
+            [0, 1, 1],
+            [1, 0, 1],
+            [1, 1, 0]
         };
 
         double[,] weightsInputHidden = new double[hiddenLayerNeurons, inputLayerNeurons];
@@ -50,8 +40,46 @@ class Program
             {
                 double[] inputs = { data[0], data[1] };
                 double expected = data[2];
+
+                double[] hidden = new double[hiddenLayerNeurons];
+                for (int i = 0; i < hiddenLayerNeurons; i++)
+                {
+                hidden[i] = biasHidden[i];
+                for (int j = 0; j < inputLayerNeurons; j++)
+                hidden[i] += inputs[j] * weightsInputHidden[i, j];
+                hidden[i] = Sigmoid(hidden[i]);
+                }
+
+                double output = biasOutput;
+                for (int i = 0; i < hiddenLayerNeurons; i++)
+                output += hidden[i] * weightsHiddenOutput[i];
+                output = Sigmoid(output);
+                }
+                }
+
+                // co z tego wyjdzie
+                foreach (var data in xorTrainingSet)
+                {
+                double[] inputs = { data[0], data[1] };
+
+                double[] hidden = new double[hiddenLayerNeurons];
+                for (int i = 0; i < hiddenLayerNeurons; i++)
+                {
+                hidden[i] = biasHidden[i];
+                for (int j = 0; j < inputLayerNeurons; j++)
+                hidden[i] += inputs[j] * weightsInputHidden[i, j];
+                hidden[i] = Sigmoid(hidden[i]);
             }
+
+            double output = biasOutput;
+            for (int i = 0; i < hiddenLayerNeurons; i++)
+                output += hidden[i] * weightsHiddenOutput[i];
+                output = Sigmoid(output);
+
+                Console.WriteLine($"{inputs[0]} XOR {inputs[1]} = {Math.Round(output)} ({output:F4})");
         }
-    }
+
+        }
 
 }
+
